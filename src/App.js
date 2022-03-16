6050c001b3c32d4f07380e9ebc2b4e6e8c9673d0
codessi <johansuh@gmail.com>
2022-03-16 12:57:43 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 
 import './App.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faPlus, faUser } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import data from './data'
+import Questions from './Questions';
 
 function App() {
 
-  const handleExpend = () => {
-    console.log('handle exp')
-  }
+
   return (
     <>
     <div className="container">
-      <div className="title">Questions And Answers About Login
-      </div>
+      <h4 className="title">Questions And Answers About Login
+      </h4>
       <ul>
-        <li className='question'>
-          <p>Do I have to allow the use of cookes?</p>
-          <button onClick= {handleExpend} className ="btn" href="#"><FontAwesomeIcon icon={faPlus}/></button>
-        </li>
+      {data.map((el, i) => {
+        return (
+          <Questions el= {el} i= {i}  />
+        )
+      })}
+        
         
       </ul>
     </div> 
---
+++ src/Questions.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+
+
+const Questions = ({i, el}) => {
+  const [open, setOpen] = useState(false)
+
+  const handleExpend = () => {
+    setOpen(!open)
+    console.log('handle exp')
+  }
+
+  return (
+    <li className='question' key={i}>
+          <div className="question-wrapper">
+            <h4>{el.title}</h4>
+            <button onClick= {handleExpend} className ="btn" href="#"><FontAwesomeIcon icon={open ? faMinus : faPlus  }/></button>
+          </div>   
+          <p className={open? "info open":"info" }> {el.info}</p>  
+
+        </li>
+  )
+}
+
+export default Questions
